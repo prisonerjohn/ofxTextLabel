3d4c273e8af72680e4fcbbc9a823f864ddde1abd
Elie Zananiri <prisonerjohn@gmail.com>
2014-07-10 14:53:48 -0400

    Replaced inline functions by ofxTextLabel class. The class has static methods to replicate the original functions, but also holds all the attributes necessary to make this multi-line thing work.

---
+++ src/ofxTextLabel.cpp
@@ -0,0 +1,243 @@
+//
+//  ofxTextLabel.cpp
+//  ofxTextLabel
+//
+//  Created by Elie Zananiri on 2014-07-10.
+//
+//
+
+#include "ofxTextLabel.h"
+
+//--------------------------------------------------------------
+template<typename T>
+void ofxTextLabel_<T>::stringToLines(T& font, const string& str, float lineLength, vector<string>& textLines, ofRectangle& textBounds, float lineSpacing)
+{
+    textLines.clear();
+    
+    vector<string> lines = ofSplitString(str, "\n", true, true);
+    for (int i = 0; i < lines.size(); i++) {
+        vector<string> words = ofSplitString(lines[i], " ", true, true);
+        
+        if (words.empty()) continue;
+        
+        string currLine = "";
+        int w = 0;
+        while (w < words.size()) {
+            if (currLine.empty()) {
+                // Add a first word to the line, regardless of length.
+                currLine = words[w];
+                w++;
+            }
+            else if (font.stringWidth(currLine + " " + words[w]) < lineLength) {
+                // It fits, add another word to the line.
+                currLine += " " + words[w];
+                w++;
+            }
+            else {
+                // It doesn't fit, add a new line and try again.
+                textLines.push_back(currLine);
+                currLine = "";
+            }
+        }
+        
+        // Add the last computed line.
+        textLines.push_back(currLine);
+    }
+    
+    // Calculate the bounds.
+    textBounds.width = 0;
+    textBounds.height = font.getLineHeight() * lineSpacing * (textLines.size() - 1);
+    for (int i = 0; i < textLines.size(); i++) {
+        textBounds.width = MAX(textBounds.width, font.stringWidth(textLines[i]));
+        if (i == textLines.size() - 1) {
+            textBounds.height += font.stringHeight(textLines[i]);
+        }
+    }
+}
+
+//--------------------------------------------------------------
+template<typename T>
+void ofxTextLabel_<T>::drawLines(T& font, vector<string>& textLines, ofRectangle& textBounds, const ofRectangle& drawBounds, float lineSpacing, ofAlignHorz alignHorz, ofAlignVert alignVert)
+{
+    if (alignHorz == OF_ALIGN_HORZ_LEFT) {
+        textBounds.x = drawBounds.x;
+    }
+    else if (alignHorz == OF_ALIGN_HORZ_CENTER) {
+        textBounds.x = drawBounds.x + (drawBounds.width - textBounds.width) / 2.0f;
+    }
+    else {  // (alignHorz == OF_ALIGN_HORZ_RIGHT)
+        textBounds.x = drawBounds.x + drawBounds.width - textBounds.width;
+    }
+    if (alignVert == OF_ALIGN_VERT_TOP) {
+        textBounds.y = drawBounds.y;
+    }
+    else if (alignVert == OF_ALIGN_VERT_CENTER) {
+        textBounds.y = drawBounds.y + (drawBounds.height - textBounds.height) / 2.0f;
+    }
+    else {  // (alignVert == OF_ALIGN_VERT_BOTTOM)
+        textBounds.y = drawBounds.y + drawBounds.height - textBounds.height;
+    }
+    
+    float lineX, lineY;
+    ofRectangle lineBounds;
+    for (int i = 0; i < textLines.size(); i++) {
+        lineBounds = font.getStringBoundingBox(textLines[i], 0, 0);
+        if (alignHorz == OF_ALIGN_HORZ_LEFT) {
+            lineX = textBounds.x;
+        }
+        else if (alignHorz == OF_ALIGN_HORZ_CENTER) {
+            lineX = textBounds.x + (textBounds.width - lineBounds.width) / 2.0f;
+        }
+        else {  // (alignHorz == OF_ALIGN_HORZ_RIGHT)
+            lineX = textBounds.x + textBounds.width - lineBounds.width;
+        }
+        lineY = textBounds.y + font.getLineHeight() * lineSpacing * i + lineBounds.height;
+        
+        font.drawString(textLines[i], lineX, lineY);
+    }
+}
+
+//--------------------------------------------------------------
+template<typename T>
+void ofxTextLabel_<T>::drawString(T& font, const string& str, const ofRectangle& drawBounds, vector<string>& textLines, ofRectangle& textBounds, float lineSpacing, ofAlignHorz alignHorz, ofAlignVert alignVert)
+{
+    stringToLines(font, str, drawBounds.width, textLines, textBounds, lineSpacing);
+    drawLines(font, textLines, textBounds, drawBounds, lineSpacing, alignHorz, alignVert);
+}
+
+//--------------------------------------------------------------
+template<typename T>
+ofxTextLabel_<T>::ofxTextLabel_()
+: _font(NULL)
+, _text("")
+, _drawBounds(0, 0, 0, 0)
+, _lineSpacing(1.0f)
+, _alignHorz(OF_ALIGN_HORZ_LEFT)
+, _alignVert(OF_ALIGN_VERT_TOP)
+, _bNeedsRebuild(false)
+{
+    
+}
+
+//--------------------------------------------------------------
+template<typename T>
+ofxTextLabel_<T>::~ofxTextLabel_()
+{
+
+}
+
+//--------------------------------------------------------------
+template<typename T>
+void ofxTextLabel_<T>::setup(T * font, const string& text, float lineLength, float lineSpacing, ofAlignHorz alignHorz, ofAlignVert alignVert)
+{
+    setup(font, text, ofRectangle(0, 0, lineLength, 0), lineSpacing, alignHorz, alignVert);
+}
+
+//--------------------------------------------------------------
+template<typename T>
+void ofxTextLabel_<T>::setup(T * font, const string& text, const ofRectangle& drawBounds, float lineSpacing, ofAlignHorz alignHorz, ofAlignVert alignVert)
+{
+    setFont(font);
+    setText(text);
+    setDrawBounds(drawBounds);
+    setLineSpacing(lineSpacing);
+    setAlignHorz(alignHorz);
+    setAlignVert(alignVert);
+}
+
+//--------------------------------------------------------------
+template<typename T>
+void ofxTextLabel_<T>::rebuild()
+{
+    stringToLines(*_font, _text, _drawBounds.width, _textLines, _textBounds, _lineSpacing);
+}
+
+//--------------------------------------------------------------
+template<typename T>
+void ofxTextLabel_<T>::update()
+{
+    if (_bNeedsRebuild) {
+        rebuild();
+        _bNeedsRebuild = false;
+    }
+}
+
+//--------------------------------------------------------------
+template<typename T>
+void ofxTextLabel_<T>::draw()
+{
+    drawLines(*_font, _textLines, _textBounds, _drawBounds, _lineSpacing, _alignHorz, _alignVert);
+}
+
+//--------------------------------------------------------------
+template<typename T>
+void ofxTextLabel_<T>::setFont(T * font)
+{
+    if (_font != font) {
+        _font = font;
+        _bNeedsRebuild = true;
+    }
+}
+
+//--------------------------------------------------------------
+template<typename T>
+void ofxTextLabel_<T>::setText(const string& text)
+{
+    if (_text != text) {
+        _text = text;
+        _bNeedsRebuild = true;
+    }
+}
+
+//--------------------------------------------------------------
+template<typename T>
+void ofxTextLabel_<T>::setLineLength(float lineLength)
+{
+    if (_drawBounds.width != lineLength) {
+        _drawBounds.width = lineLength;
+        _bNeedsRebuild = true;
+    }
+}
+
+//--------------------------------------------------------------
+template<typename T>
+void ofxTextLabel_<T>::setDrawBounds(const ofRectangle& drawBounds)
+{
+    if (_drawBounds != drawBounds) {
+        _drawBounds = drawBounds;
+        _bNeedsRebuild = true;
+    }
+}
+
+//--------------------------------------------------------------
+template<typename T>
+void ofxTextLabel_<T>::setLineSpacing(float lineSpacing)
+{
+    if (_lineSpacing != lineSpacing) {
+        _lineSpacing = lineSpacing;
+        _bNeedsRebuild = true;
+    }
+}
+
+//--------------------------------------------------------------
+template<typename T>
+void ofxTextLabel_<T>::setAlignHorz(ofAlignHorz alignHorz)
+{
+    if (_alignHorz != alignHorz) {
+        _alignHorz = alignHorz;
+        _bNeedsRebuild = true;
+    }
+}
+
+//--------------------------------------------------------------
+template<typename T>
+void ofxTextLabel_<T>::setAlignVert(ofAlignVert alignVert)
+{
+    if (_alignVert != alignVert) {
+        _alignVert = alignVert;
+        _bNeedsRebuild = true;
+    }
+}
+
+//--------------------------------------------------------------
+template class ofxTextLabel_<ofTrueTypeFont>;
---
+++ src/ofxTextLabel.h
@@ -0,0 +1,76 @@
+//
+//  ofxTextLabel.h
+//  ofxTextLabel
+//
+//  Created by Elie Zananiri on 2014-07-10.
+//
+//
+
+#pragma once
+
+#include "ofMain.h"
+
+//--------------------------------------------------------------
+template<typename T>
+class ofxTextLabel_
+{
+public:
+    static void stringToLines(T& font, const string& str, float lineLength, vector<string>& textLines, ofRectangle& textBounds, float lineSpacing = 1.0f);
+    static void drawLines(T& font, vector<string>& textLines, ofRectangle& textBounds, const ofRectangle& drawBounds, float lineSpacing = 1.0f, ofAlignHorz alignHorz = OF_ALIGN_HORZ_LEFT, ofAlignVert alignVert = OF_ALIGN_VERT_TOP);
+    static void drawString(T& font, const string& str, const ofRectangle& drawBounds, vector<string>& textLines, ofRectangle& textBounds, float lineSpacing = 1.0f, ofAlignHorz alignHorz = OF_ALIGN_HORZ_LEFT, ofAlignVert alignVert = OF_ALIGN_VERT_TOP);
+    
+    ofxTextLabel_();
+    ~ofxTextLabel_();
+    
+    void setup(T * font, const string& text, float lineLength, float lineSpacing = 1.0f, ofAlignHorz alignHorz = OF_ALIGN_HORZ_LEFT, ofAlignVert alignVert = OF_ALIGN_VERT_TOP);
+    void setup(T * font, const string& text, const ofRectangle& drawBounds, float lineSpacing = 1.0f, ofAlignHorz alignHorz = OF_ALIGN_HORZ_LEFT, ofAlignVert alignVert = OF_ALIGN_VERT_TOP);
+    
+    virtual void rebuild();
+    virtual void update();
+    virtual void draw();
+    
+    void setFont(T * font);
+    T * getFont() { return _font; }
+    
+    void setText(const string& text);
+    const string& getText() { return _text; }
+    
+    void setLineLength(float lineLength);
+    float getLineLength() { return _drawBounds.width; }
+    
+    void setDrawBounds(const ofRectangle& drawBounds);
+    const ofRectangle& getDrawBounds() {  return _drawBounds; }
+    
+    void setLineSpacing(float lineSpacing);
+    float getLineSpacing() { return _lineSpacing; }
+
+    void setAlignHorz(ofAlignHorz alignHorz);
+    ofAlignHorz getAlignHorz() { return _alignHorz; }
+    
+    void setAlignVert(ofAlignVert alignVert);
+    ofAlignVert getAlignVert() { return _alignVert; }
+    
+    const vector<string>& getTextLines() { return _textLines; }
+    const ofRectangle& getTextBounds() { return _textBounds; }
+    
+protected:
+    T * _font;
+    string _text;
+    ofRectangle _drawBounds;
+    
+    float _lineSpacing;
+    ofAlignHorz _alignHorz;
+    ofAlignVert _alignVert;
+        
+    vector<string> _textLines;
+    ofRectangle _textBounds;
+    
+    bool _bNeedsRebuild;
+};
+
+//--------------------------------------------------------------
+typedef ofxTextLabel_<ofTrueTypeFont> ofxTextLabel;
+
+// Include the cpp so that we can use the template with non-core font types.
+// Make sure not to include the cpp in the target sources!
+#include "ofxTextLabel.cpp"
